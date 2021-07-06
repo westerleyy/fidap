e92cc70b8184c1ca6bb4ff360946a31286a05b10
Wesley Chioh <wes.chioh@outlook.com>
2021-07-06 23:25:21 +0800

    updated streamlit mvp and public holidays notebook

--- streamlit_mvp.py
+++ streamlit_mvp.py
@@ -9,6 +9,8 @@ import numpy as np
 import pandas as pd
 import fidap 
 from config import api_key
+import matplotlib.pyplot as plt
+import altair as alt
 
 # instantiate api connection
 fidap = fidap.fidap_client(api_key=api_key)
@@ -16,12 +18,26 @@ fidap = fidap.fidap_client(api_key=api_key)
 # sample covid19 data from nyt
 
 nyt_sample = fidap.sql("""
-                       SELECT confirmed_cases, deaths
+                       SELECT confirmed_cases, deaths, date, state_name
                        FROM bigquery-public-data.covid19_nyt.us_states
-                       LIMIT 10;                      
+                       WHERE state_name IN ('New York', 'New Jersey', 'Connecticut');                      
                        """
     )
 
+states_boundaries = fidap.sql("""
+                              SELECT state_fips_code, state, state_name, state_geom, int_point_lat AS lat, int_point_lon AS lon
+                              FROM bigquery-public-data.geo_us_boundaries.states;
+                             """)
 
+c = alt.Chart(nyt_sample).mark_line().encode(
+    x = 'date',
+    y = 'confirmed_cases',
+    color = 'state_name',
+    strokeDash = 'state_name'
+    )
 
-st.line_chart(nyt_sample)
+st.altair_chart(c, use_container_width = True)
+
+# it appears that streamlit cannot support polygons for mapping.
+st.map(states_boundaries)
+
