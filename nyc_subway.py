5435667eec6cd1432eb617d7f461368bf5a18cd4
Wesley Chioh <wes.chioh@outlook.com>
2021-07-18 12:32:15 +0800

    updated streamlit to ingest data from fidap

--- nyc_subway.py
+++ nyc_subway.py
@@ -10,7 +10,25 @@ import numpy as np
 import pandas as pd
 import altair as alt
 from datetime import datetime
+from config import api_key
+import fidap
 
+# instantiate api connection
+fidap = fidap.fidap_client(api_key=api_key)
+
+
+# get us holidays
+us_holidays = fidap.sql("""
+SELECT holidayName, SUBSTR(CAST(ph.date AS STRING),1,10) AS holidayDate   
+FROM fidap-301014.azure_open_datasets.public_holidays AS ph
+WHERE EXTRACT(YEAR FROM ph.date) BETWEEN 2019 AND 2021
+AND ph.countryRegionCode = 'US'
+                        """)
+
+us_holidays['holidayDate'] = pd.to_datetime(us_holidays['holidayDate'], format = '%Y-%m-%d')
+us_holidays['holidayDate'] = us_holidays['holidayDate'].dt.normalize()
+
+# read nyc turnstile data
 nyc_turnstile_data_2019 = pd.read_csv('C:/Users/wesch/OneDrive/Documents/Fidap/fidap/nyc_mta_transit_data/body_2019.csv', usecols = ['stop_name', 'daytime_routes', 'line', 'gtfs_longitude', 'gtfs_latitude', 'date', 'entries', 'exits'])
 nyc_turnstile_data_2020 = pd.read_csv('C:/Users/wesch/OneDrive/Documents/Fidap/fidap/nyc_mta_transit_data/body_2020.csv', usecols = ['stop_name', 'daytime_routes', 'line', 'gtfs_longitude', 'gtfs_latitude', 'date', 'entries', 'exits'])
 nyc_turnstile_data_2021 = pd.read_csv('C:/Users/wesch/OneDrive/Documents/Fidap/fidap/nyc_mta_transit_data/body_2021.csv', usecols = ['stop_name', 'daytime_routes', 'line', 'gtfs_longitude', 'gtfs_latitude', 'date', 'entries', 'exits'])
@@ -34,9 +52,12 @@ nyc_turnstile_data_2021 = initial_df_processing(nyc_turnstile_data_2021)
 station_geom = nyc_turnstile_data_2020.copy().reset_index()[['stop_name', 'longitude', 'latitude']].drop_duplicates()
 
 # get station list
-
 station_list = []
 station_list = station_geom.stop_name.unique().tolist()
+
+# get holiday list
+holiday_list = []
+holiday_list = us_holidays.holidayName.unique().tolist()
 
 header = st.beta_container()
 sidebar_input = st.beta_container()
@@ -47,7 +68,7 @@ with header:
     st.markdown("""
                 **Wesley Chioh**  
             
-                This web app will utilize turnstile information from NYC MTA, meticulously cleaned and by [Chris Whong](https://qri.cloud/nyc-transit-data/turnstile_daily_counts_2020) for the years 2019 - 2021.  
+                This web app will utilize holiday data from the [Fidap platform](https://app.fidap.com), turnstile information from NYC MTA, meticulously cleaned and by [Chris Whong](https://qri.cloud/nyc-transit-data/turnstile_daily_counts_2020) for the years 2019 - 2021.  
               
                 What a year 2020 was. We all know that ridership decreased, but by how much? And has it rebounded? How much did it decline year on year anyway?   
                    
@@ -58,7 +79,7 @@ with header:
 
 with sidebar_input:
     
-    st.sidebar.header("MTA 2019 - 2021")
+    st.sidebar.header("MTA and Holidays")
 
     station1 = st.sidebar.selectbox('Station 1:', station_list)
     # station2 = st.sidebar.selectbox('Station 2:', station_list)
@@ -74,6 +95,9 @@ with sidebar_input:
     
     start_date = np.datetime64(start_date)
     end_date = np.datetime64(end_date)
+    
+    # select holidays 
+    holidays = st.sidebar.selectbox('Public Holiday:', holiday_list)
 
     # creating df and geom for stations
     station1_df = nyc_turnstile_data_2020[(nyc_turnstile_data_2020['stop_name'] == station1) & (nyc_turnstile_data_2020['date'] >= start_date) & (nyc_turnstile_data_2020['date'] <= end_date)].copy()
@@ -95,6 +119,13 @@ with sidebar_input:
     station_2019_df = station_2019_df.melt(id_vars = ['stop_name', 'daytime_routes', 'line', 'longitude', 'latitude', 'date'], value_vars = ['entries', 'exits'])
     station_2019_df['value'] = station_2019_df.value.astype(int)
     
+    # get holidays and ridership accordingly 
+    
+    selected_holidays = us_holidays[(us_holidays['holidayName'] == holidays)]
+    ridership_holiday_2019 = station_2019_df.merge(selected_holidays, left_on = 'date', right_on = 'holidayDate', how = 'inner' )
+    ridership_holiday_2020 = station1_df.merge(selected_holidays, left_on = 'date', right_on = 'holidayDate', how = 'inner' )
+    combined_ridership_holiday = pd.concat([ridership_holiday_2019, ridership_holiday_2020])
+    
     # get 2021 equivalent
     
     start_date_2021 = start_date + np.timedelta64(365, 'D')
@@ -105,6 +136,11 @@ with sidebar_input:
         station_2021_df = nyc_turnstile_data_2021[(nyc_turnstile_data_2021['stop_name'] == station1) & (nyc_turnstile_data_2021['date'] >= start_date_2021) & (nyc_turnstile_data_2021['date'] <= end_date_2021)].copy()
         station_2021_df = station_2021_df.melt(id_vars = ['stop_name', 'daytime_routes', 'line', 'longitude', 'latitude', 'date'], value_vars = ['entries', 'exits'])
         station_2021_df['value'] = station_2021_df.value.astype(int)
+        ridership_holiday_2021 = station_2021_df.merge(selected_holidays, left_on = 'date', right_on = 'holidayDate', how = 'inner' )
+        combined_ridership_holiday = pd.concat([combined_ridership_holiday, ridership_holiday_2021])
+
+    combined_ridership_holiday['year'] = combined_ridership_holiday['date'].dt.year
+    
 
 with charts:
     
@@ -119,7 +155,8 @@ with charts:
                   
                 """)
     
-    c1 = alt.Chart(station1_df).mark_line().encode(
+    c1 = alt.Chart(station1_df,
+                   title = '2020 Ridership').mark_line().encode(
         x = 'date',
         y = 'value',
         color = 'variable',
@@ -133,7 +170,8 @@ with charts:
                   
                 """)
 
-    c2 = alt.Chart(station_2019_df).mark_line().encode(
+    c2 = alt.Chart(station_2019_df,
+                   title = '2019 Ridership').mark_line().encode(
         x = 'date',
         y = 'value',
         color = 'variable',
@@ -150,7 +188,8 @@ with charts:
                 """)
 
         
-        c3 = alt.Chart(station_2021_df).mark_line().encode(
+        c3 = alt.Chart(station_2021_df,
+                       title = '2021 Ridership').mark_line().encode(
             x = 'date',
             y = 'value',
             color = 'variable',
@@ -159,6 +198,17 @@ with charts:
             ).interactive()
     
         st.altair_chart(c3, use_container_width = True)
+    
+    c4 = alt.Chart(combined_ridership_holiday,
+                   title = 'Holiday Ridership',
+                   width = 200).mark_bar().encode(
+                       x = 'variable',
+                       y = 'value',
+                       color = 'variable',
+                       column = 'year'
+                       )
+                       
+    c4                 
         
         
         
