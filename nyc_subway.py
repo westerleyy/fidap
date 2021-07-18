a609caacf10dfecd6295be210b88a8df6ed86f90
Wesley Chioh <wes.chioh@outlook.com>
2021-07-18 12:46:42 +0800

    updated fidap

--- nyc_subway.py
+++ nyc_subway.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 import altair as alt
 from datetime import datetime
-from config import api_key
+# from config import api_key
 import fidap
 
 # instantiate api connection
